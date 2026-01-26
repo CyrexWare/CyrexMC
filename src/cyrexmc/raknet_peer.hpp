72ac99d8413ab1714154c91f6cb61a4354a07aac
Jamie <jamiependergast@gmail.com>
2026-01-26 18:46:48 +0000

    removed last const from declaration

--- src/cyrexmc/raknet_peer.hpp
+++ src/cyrexmc/raknet_peer.hpp
@@ -11,7 +11,7 @@ class RakNetPeer : public INetworkPeer
 public:
     explicit RakNetPeer(RakNet::RakPeerInterface* peerInterface);
     [[nodiscard]] StartupResult startup(StartupInfo startupInfo) override;
-    void shutdown(const ShutdownInfo shutdownInfo) override;
+    void shutdown(ShutdownInfo shutdownInfo) override;
     void setMaximumIncomingConnections(std::uint16_t maxIncomingConnections) override;
     [[nodiscard]] Packet* receive() override;
     [[nodiscard]] std::uint32_t send(const char* data, std::uint32_t length, SendInfo sendInfo) override;
