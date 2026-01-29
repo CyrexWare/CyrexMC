24234c31065931c1a5ba24a2aa2c181aeb3362fe
lapinozz <monstrefou@gmail.com>
2026-01-29 14:43:41 -0500

    fix compilation

--- src/log/console_logger.hpp
+++ src/log/console_logger.hpp
@@ -1,10 +1,11 @@
 #pragma once
+
 #include "message_format.hpp"
 #include "text/render/ansi.hpp"
 
 #include <iostream>
 
-namespace cyrex::logx
+namespace cyrex::log
 {
 inline void sendConsoleMessage(MessageType type, std::string_view message)
 {
