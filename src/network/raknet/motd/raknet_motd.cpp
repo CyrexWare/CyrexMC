5497aeaf3a5c7ec8c1d32bf6837cafe1c520c875
Toxic <diddyofficially@gmail.com>
2026-02-06 12:00:04 +0200

    correct

--- src/network/raknet/motd/raknet_motd.cpp
+++ src/network/raknet/motd/raknet_motd.cpp
@@ -3,7 +3,7 @@
 #include "network/mcbe/protocol/protocol_info.hpp"
 #include "network/mcbe/protocol/types/GameMode.hpp"
 
-std::string cyrex::network::raknet::buildMotd(const cyrex::Server& server)
+std::string cyrex::nw::raknet::buildMotd(const cyrex::Server& server)
 {
     auto gm = server.getDefaultGameMode();
 
